c4848e68264fed1b9c237f5b62d200423c8d584c
Ipsita Bhattacharya <i.bhatya@gmail.com>
2023-12-26 16:45:28 +0530

    about completed

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -129,12 +129,13 @@ const About = () => {
     'JavaScript',
     'TypeScript',
     'React',
-    'Next',
+    'NextJS',
     'AngularJS',
     'NodeJS',
     'Python',
     'MongoDB',
     'PostgreSql',
+    'Docker',
   ];
 
   return (
@@ -146,18 +147,16 @@ const About = () => {
           <div>
             <p>
               Hello! My name is Ipsita and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2015 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              computer science journey began in school, where I initially encountered coding
+              concepts. Inspired by coursework, I delved deeper into programming, sparking a lasting
+              passion for technology.
             </p>
 
             <p>
               Fast-forward to today, and I’ve had the privilege of working at{' '}
               <a href="https://www.rmgx.in/">RMgX Technologies LLP</a> as a Software Developer
-              Intern. There I've collaborated on 4+ client projects, contributing 100+ times with
-              expertise in ReactJS, NextJS, AngularJS, TypeScript, NodeJS, MUI, and more. I excel in
-              SDLC, implementing React-Redux for efficient, reusable code, earning praise for
-              on-time delivery and dedication from both team and clients.
+              Intern. There I gained invaluable experience, contributing to projects and honing my
+              skills in a dynamic and innovative environment.
               {/* <a href="https://starry.com/">a start-up</a>,{' '}
               <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
               <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
@@ -167,13 +166,19 @@ const About = () => {
             </p>
 
             <p>
+              I have also solved algorithmic challenges on{' '}
+              <a href="https://leetcode.com/ipsita04/">LeetCode </a>, enhancing problem-solving
+              skills and algorithm proficiency through consistent and strategic coding practice.
+            </p>
+
+            {/* <p>
               I also recently{' '}
               <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
                 launched a course
               </a>{' '}
               that covers everything you need to build a web app with the Spotify API using Node
               &amp; React.
-            </p>
+            </p> */}
 
             <p>Here are a few technologies I’ve been working with recently:</p>
           </div>
